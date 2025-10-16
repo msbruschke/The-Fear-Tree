0c158c3cfdbb937b0439f02f91af1dfac5edb3c7
msbruschke <103210835+msbruschke@users.noreply.github.com>
2025-10-15 17:03:19 -0700

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -41,10 +41,11 @@ function canGenPoints(){
 function getPointGen() {
 	if(!canGenPoints())
 		return new Decimal(0)
+	
+	let gain = new Decimal(1)
 
 	if (hasUpgrade('p', 11)) gain = gain.times(2)
 	
-	let gain = new Decimal(1)
 	return gain
 }
 
